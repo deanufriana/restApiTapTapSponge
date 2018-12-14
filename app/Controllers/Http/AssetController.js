fd8df9e255a162dab35f61dfbd2d3a2797c21bac
mysilkyheart <deanheart09@gmail.com>
2018-12-14 08:51:32 +0700

    uploadImage on Cloudinary

--- app/Controllers/Http/AssetController.js
+++ app/Controllers/Http/AssetController.js
@@ -1,11 +1,34 @@
 'use strict'
 const Assets = use('App/Models/Asset')
 const Helpers = use('Helpers')
+const CloudinaryService = use('App/Services/CloudinaryService');
 
 class AssetController {
 
     async index() {
         return await Assets.all()
+    }
+
+    async mainImage({ request, response, session }) {
+        const file = request.file('mainImage');
+        try {
+
+            const cloudinaryResponse = await CloudinaryService.v2.uploader.upload(file.tmpPath, { folder: 'postsapp' });
+            
+            const post = await Assets.find(1);
+            post.mainImage = cloudinaryResponse.secure_url;
+            
+            await post.save();
+            
+            session.flash({ success: 'Successfully added post' });
+            
+            return response.redirect('back');
+        } catch (e) {
+            
+            session.flash({ error: 'Error Uploading Image' });
+            
+            return response.redirect('/')
+        }
     }
 
     async backgroundColor({ request, response }) {
@@ -15,13 +38,13 @@ class AssetController {
         return response.redirect('/adminPage')
     }
 
-    async mainImage({ request, response }) {
+   /*  async mainImage({ request, response }) {
         const post = await Assets.find(1)
-                
+
         const Image = request.file('mainImage', {
             types: ['image'],
             size: '2mb',
-            fileName:'mainImage.jpg',
+            fileName: 'mainImage.jpg',
         })
 
         await Image.move(Helpers.tmpPath('assets'), {
@@ -38,12 +61,12 @@ class AssetController {
         return response.redirect('/adminPage')
     }
 
-    async ruleGame({request, response}) {
+    async ruleGame({ request, response }) {
         const post = await Assets.find(1)
         post.setRule = request.input('ruleGame')
         await post.save()
         return response.redirect('/adminPage')
-    }
+    } */
 }
 
 module.exports = AssetController
--- app/Controllers/Http/SetruleController.js
+++ app/Controllers/Http/SetruleController.js
@@ -1,6 +1,7 @@
 'use strict'
 const Setrule = use('App/Models/Setrule')
 const Helpers = use('Helpers')
+const CloudinaryService = use('App/Services/CloudinaryService')
 
 class SetruleController {
 
@@ -8,35 +9,51 @@ class SetruleController {
         return await Setrule.all()
     }
 
-    async post({ request, response }) {
+    /*     async mainImage({ request, response, session }) {
+            const file = request.file('mainImage');
+            try {
+                const cloudinaryResponse = await CloudinaryService.v2.uploader.upload(file.tmpPath, { folder: 'postsapp' });
+                const post = await Assets.find(1);
+                post.mainImage = cloudinaryResponse.secure_url;
+                await post.save();
+                session.flash({ success: 'Successfully added post' });
+                return response.redirect('back');
+            } catch (e) {
+                session.flash({ error: 'Error Uploading Image' });
+                return response.redirect('/')
+            }
+        } */
 
-        const post = new Setrule()
+    async post({ request, response, session }) {
 
-        const btnImage = request.file('buttonImage', {
-            types: ['image'], 
-            size: '2mb', 
+        const ImageButton = request.file('buttonImage', {
+            types: ['image'],
+            size: '2mb',
             fileName: `${new Date().getTime()}.jpg`,
         })
-        const soundButton = request.file('soundButton', {
-            fileName: `${new Date().getTime()}.jpg`
-        })
+        const soundButton = request.file('soundButton')
 
-        await btnImage.move(Helpers.tmpPath('button'), {
-            name: `${new Date().getTime()}`,
-            overwrite: true
-        })
+        try {
 
-        await soundButton.move(Helpers.tmpPath('sound'), {
-            name: `${new Date().getTime()}`,
-            overwrite: true
-        })
+            const buttonResponse = await CloudinaryService.v2.uploader.upload(ImageButton.tmpPath, { folder: 'imageButton' });
 
-        post.ImageButton = btnImage.fileName
-        post.textButton = request.input('textButton')
-        post.soundButton = soundButton.fileName
+            const soundResponse = await CloudinaryService.v2.uploader.upload(soundButton.tmpPath, { folder: 'soundButton', resource_type: 'video', });
 
-        await post.save()
-        return response.redirect('/adminPage')
+            const post = new Setrule()
+            post.ImageButton = buttonResponse.secure_url;
+            post.soundButton = soundResponse.secure_url;
+            post.textButton = request.input('textButton')
+
+            await post.save();
+
+            session.flash({ success: 'Successfully added post' });
+
+            return response.redirect('back');
+        } catch (e) {
+            session.flash({ error: 'Error Uploading Image' });
+            console.log(e)
+            return response.redirect('/')
+        }
     }
 
 }
